cfb2244d5652dafea629dc97b3d4471838268075
Vegar <vegar@epost.no>
2026-03-14 15:51:42 +0100

    Fix: Google Analytics via extra_javascript (Material theme)

---
+++ docs/assets/javascripts/analytics.js
@@ -0,0 +1,9 @@
+<!-- Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-KCYT87ZMVZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-KCYT87ZMVZ');
+</script>
